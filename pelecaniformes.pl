a72cdab9fd3157d3d714d584964f35920416fdca
croatis <christianroatis@Christians-MacBook-Pro.local>
2017-04-01 14:46:40 -0600

    first commit

---
+++ pelecaniformes.pl
@@ -0,0 +1,6 @@
+order(pelicans, pelecaniformes).
+order(herons, pelecaniformes).
+order(ibises, pelecaniformes).
+order(allies, pelecaniformes).
+
+isPelecaniformes(X) :- order(X, pelecaniformes).# 449_Prolog
