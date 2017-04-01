569f1a3a6c017f280e5268cf7f7f0dc29ddd38a9
Christian Roatis <christian.roatis@ucalgary.ca>
2017-04-01 16:54:44 -0700

    Implemented first 6 predicates; framework of 7th

--- pelecaniformes.pl
+++ pelecaniformes.pl
@@ -8,15 +8,13 @@ ofOrder(threskiornithdae, pelecaniformes).
 
 % Read as: X is of gamily Y.
 % Paragraphs seperated by ofFamily, "general" or "english" ofFamily name specified in comments.
-ofFamily(pelican, pelecanidae).
 ofFamily(pelecanus, pelecanidae).
 
 % bitterns
-ofFamily(bittern, ardeidae).
 ofFamily(botaurus, ardeidae).
 ofFamily(ixobrychus, ardeidae).
+
 % herons
-ofFamily(heron, ardeidae).
 ofFamily(ardea, ardeidae). 
 ofFamily(egretta, ardeidae). % can be a heron or an egret
 ofFamily(butorides, ardeidae). 
@@ -37,7 +35,7 @@ ofFamily(spoonbill, threskiornithdae).
 ofFamily(platalea, threskiornithdae).
 
 
-% Read as: X is of genus Y. No common names specified for genus. Spaced out by family.
+% Read as: X is of genus Y. No common names specified for genus. Spaced out by genus.
 ofGenus(erythrorhynchos, pelecanus).
 ofGenus(occidentalis, pelecanus).
 
@@ -59,6 +57,131 @@ ofGenus(falcinellus, plegadis).
 ofGenus(chihi, plegadis).
 ofGenus(ajaja, platalea).
 
+% Common names; spaced out by common name.
+commonName(pelecanus, pelican).
+
+commonName(botaurus, bittern).
+commonName(ixobrychus, bittern).
+
+commonName(ardea, heron).
+commonName(egretta, heron).
+commonName(butorides, heron).
+
+commonName(egretta, egret).
+commonName(bubulcus, egret).
+
+commonName(nycticorax, nightHeron).
+commonName(nyctanassa, nightHeron).
+
+commonName(eudocimus, ibis).
+commonName(plegadis, ibis).
+
+commonName(platalea, spoonbill).
+
+% Species section
+commonName(pelecanus_erythrorhynchos, americanWhitePelican).
+commonName(pelecanus_occidentalis, brownPelican).
+commonName(botaurus_lentiginosus, americanBittern).
+commonName(ixobrychus_exilis, leastBittern).
+commonName(ardea_herodias, greatBlueHeron).
+commonName(ardea_alba, greatEgret).
+commonName(egretta_thula, snowyEgret).
+commonName(egretta_caerulea, littleBlueHeron).
+commonName(egretta_tricolor, tricoloredHeron).
+commonName(egretta_rufescens, reddishEgret).
+commonName(bubulcus_ibis, cattleEgret).
+commonName(butorides_bubulcus_virescens, greenHerron).
+commonName(nycticorax_nycticorax, blackCrownedNightHeron).
+commonName(nyctanassa_violacea, yellowCrownedNightHeron).
+commonName(eudocimus_albus, whiteIbis).
+commonName(plegadis_falcinellus, glossyIbis).
+commonName(plegadis_chihi, whiteFacedIbis).
+commonName(platalea_ajaja, roseateSpoonbill).
+
+
+% --------------------------------------------------------------------------------
 
 % Predicates to implement:
+
+order(pelecaniformes).
+
+% ----------------------------------------
+
+family(pelecanidae).
+family(ardeidae).
+family(threskiornithdae).
+
+% ----------------------------------------
+
+% Pelecanidae
+% pelicans
+genus(pelecanus).
+
+% Ardeidae
+% bitterns
+genus(botaurus).
+genus(ixobrychus).
+% herons
+genus(ardea). 
+genus(egretta). % can be a heron or an egret
+genus(butorides). 
+% egret
+genus(bubulcus, ardeidae). 
+% nightHerons
+genus(nycticorax). 
+genus(nyctanassa).
+
+% Threskiornithdae
+% ibis'
+genus(eudocimus).
+genus(plegadis).
+% spoonbills
+genus(platalea).
+
+% ----------------------------------------
+
+% Pelecanus
+species(erythrorhynchos).
+species(occidentalis).
+
+% Ardeidae
+species(lentiginosus).
+species(exilis).
+species(herodias).
+species(alba).
+species(thula).
+species(caerulea).
+species(tricolor).
+species(rufescens).
+species(ibis).
+species(virescens).
+species(nycticorax).
+species(violacea).
+
+% Threskiornithdae
+species(albus, eudocimus).
+species(falcinellus, plegadis).
+species(chihi, plegadis).
+species(ajaja, platalea).
+
+% ----------------------------------------
+
+hasParent(A, B) :- ofOrder(A, B); ofFamily(A,B); ofGenus(A, B).
+
+% ----------------------------------------
+
+hasParent2(A, B) :- ofOrder(A, B); ofFamily(A,B); ofGenus(A, B).
+
+% ----------------------------------------
+
+hasCommonName(N, C) :- commonName(N, C).
+
+% ----------------------------------------
+
+hasCommonName(G, S, C) :- genus(G), 
+							species(S), 
+								commonName(string_concat(G,S), C).
+
+
+
 
